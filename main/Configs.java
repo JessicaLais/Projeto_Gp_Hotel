604d85872ce0e56fa579a3e86de9e54ed9577a28
JessicaLais <jessicalais.freire@gmail.com>
2024-05-04 17:32:02 -0300

    Adicionado as conf, pra caso o prof queira algo a mais

---
+++ main/Configs.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Configs {
+    //pra caso queira ficar infinito
+	public static boolean GERAR_HOSPEDES = false;
+	//aumentar a qtd 
+	public static int QUANTIDADE_DE_QUARTOS = 0;
+	public static int QUANTIDADE_DE_HOSPEDES = 0;
+	public static int QUANTIDADE_DE_CAMAREIRAS = 0;
+	public static int QUANTIDADE_DE_RECEPCIONISTAS = 0;
+	
+	// Qtd máxima
+	public static int QUANTIDADE_DE_FAMILIAS = 10;
+	
+	// Probabilidade de um hospede estar sozinho
+	public static int PROBABILIDADE_SOZINHO = 10;
+	
+	// Tempo
+	public static int TEMPO_DE_LIMPEZA = 10000;
+	public static int TEMPO_PASSEIO = 15000;
+	public static int TEMPO_ESPERA = 15500;
+}
