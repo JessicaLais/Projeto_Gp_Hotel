ee61717b74990f5793142e70e5685a7f0848f4bd
JessicaLais <jessicalais.freire@gmail.com>
2024-05-04 23:26:18 -0300

    Adicionando o Básico da classe Quarto

--- main/Quarto.java
+++ main/Quarto.java
@@ -1,4 +1,20 @@
 package main;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Quarto {
-    
+	int numero;
+	boolean vago;
+	boolean limpo;
+	private List<Hospede> hospedes;
+	boolean limpando;/* "limpando" criada para evitar o grande conflito entre camareiras*/
+
+	public Quarto() {
+		this.numero = Main.quartos.size() + 1;
+		// já começa vago e limpo
+		this.vago = true;
+		this.limpo = true;
+
+		this.hospedes = new ArrayList<Hospede>();
+	}
 }
