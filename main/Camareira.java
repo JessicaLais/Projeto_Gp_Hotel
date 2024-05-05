a7a2ae7e79257c19d67b742b413a21401f587b7e
jorge igor <jorgeigor123@hotmail.com>
2024-05-05 20:21:06 -0300

    Adds cleaning loop to the Maid class

--- main/Camareira.java
+++ main/Camareira.java
@@ -1,12 +1,25 @@
 package main;
-public class Camareira{
+public class Camareira extends Thread{
 	String nome;
 	
 	public Camareira() {
 		this.nome = "Camareira" + (Main.camareiras.size() + 1);
 		this.start();
 	}
-    @Override
+	
+	@Override
 	public void run() {
+		try {
+			while(true) {
+				tentarLimpar();
+				Thread.sleep(1000); // Ela espera 1 segundo a cada tentativa
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Procura algum quarto para limpar
+	public void tentarLimpar() throws InterruptedException {
+        
     }
-}
