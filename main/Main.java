e8c2adb5a53faf27245100122b4397d7cfe6bccf
Octavio Leite Neto <otavio.gta@gmail.com>
2024-05-04 23:09:59 -0300

    inserting Main and Configs

--- main/Main.java
+++ main/Main.java
@@ -1,4 +1,30 @@
 package main;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    
+
+	public static List<Quarto> quartos;
+
+	public static List<Hospede> hospedes;
+
+	public static List<Camareira> camareiras;
+
+	public static List<Recepcionista> recepcionistas;
+
+	public static List<Integer> chaves;
+
+	public static List<Hospede> fila;
+
+	public static void main(String[] args) {
+
+
+		quartos = new ArrayList<Quarto>();
+		hospedes = new ArrayList<Hospede>();
+		camareiras = new ArrayList<Camareira>();
+		recepcionistas = new ArrayList<Recepcionista>();
+
+		chaves = new ArrayList<Integer>();
+		fila = new ArrayList<Hospede>();
+	}
 }
