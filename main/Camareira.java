41fa18fa9fa1ae80328129eaecc98924a9fbdd2a
jorge igor <jorgeigor123@hotmail.com>
2024-05-05 10:30:58 -0300

    Introduces Camareira class with dynamic name and initialization.

--- main/Camareira.java
+++ main/Camareira.java
@@ -1,4 +1,12 @@
 package main;
 public class Camareira{
-    
+	String nome;
+	
+	public Camareira() {
+		this.nome = "Camareira" + (Main.camareiras.size() + 1);
+		this.start();
+	}
+    @Override
+	public void run() {
+    }
 }
